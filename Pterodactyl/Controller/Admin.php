f3d2b58b4e1f8839666d9d247b18fdd95cb378ef
Test <93833001+fabpot-del@users.noreply.github.com>
2023-01-07 10:49:47 +0100

    Update Admin.php

--- Pterodactyl/Controller/Admin.php
+++ Pterodactyl/Controller/Admin.php
@@ -20,4 +20,36 @@ class Admin implements \Box\InjectionAwareInterface
     {
         return $this->di;
     }
+  
+  	public function fetchNavigation()
+    {
+        return array(
+            'group'  =>  array(
+                'index'     => 1600,                // menu sort order
+                'location'  => 'pterodactyl',           // menu group identificator for subitems
+                'label'     => 'Pterodactyl Module',    // menu group title
+                'class'     => 'pterodactyl',           // used for css styling menu item
+            ),
+            'subpages'=> array(
+                array(
+                    'location'  => 'pterodactyl',       // place this module in extensions group
+                    'label'     => 'Pterodactyl Configuration',
+                    'index'     => 1500,
+                    'uri'       => $this->di['url']->adminLink('pterodactyl'),
+                    'class'     => '',
+                ),
+            ),
+        );
+    }
+  
+  	public function register(\Box_App &$app)
+    {
+        $app->get('/pterodactyl',             'get_index', array(), get_class($this));
+    }
+  
+  	public function get_index(\Box_App $app)
+    {
+        $this->di['is_admin_logged'];
+        return $app->render('mod_pterodactyl_index');
+    }
 }
