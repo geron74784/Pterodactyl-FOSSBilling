dbb81238b4c4efc2f14d21529a152333468e1fac
Test <93833001+fabpot-del@users.noreply.github.com>
2023-01-07 10:26:48 +0100

    Update Service.php

--- Pterodactyl/Service.php
+++ Pterodactyl/Service.php
@@ -1,1 +1,23 @@
+<?php
+namespace Box\Mod\Pterodactyl;
 
+class Service
+{
+  	protected $di;
+
+    /**
+     * @param mixed $di
+     */
+    public function setDi($di)
+    {
+        $this->di = $di;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDi()
+    {
+        return $this->di;
+    }
+}
