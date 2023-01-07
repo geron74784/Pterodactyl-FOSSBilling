d956d58edde7327c124e9eeb67a3029267d80e44
fabpot-del <93833001+fabpot-del@users.noreply.github.com>
2023-01-07 16:42:33 +0100

    Update Service.php

--- Servicepterodactyl/Service.php
+++ Servicepterodactyl/Service.php
@@ -20,4 +20,18 @@ class Service implements \Box\InjectionAwareInterface
     {
         return $this->di;
     }
+  
+    /**
+     * Method to uninstall module.
+     *
+     * @return bool
+     * @throws \Box_Exception
+     */
+	    public function uninstall()
+    {
+		$this->di['db']->exec("DROP TABLE IF EXISTS `service_pterodactyl`");
+		$this->di['db']->exec("DROP TABLE IF EXISTS `service_pterodactyl_server`");
+        //throw new \Box_Exception("Throw exception to terminate module uninstallation process with a message", array(), 124);
+        return true;
+    }
 }
