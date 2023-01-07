b5f6e26a1ae27587a8f8395a30ae8fc8e3175c49
Test <93833001+fabpot-del@users.noreply.github.com>
2023-01-07 10:22:12 +0100

    Update Admin.php

--- Pterodactyl/Controller/Admin.php
+++ Pterodactyl/Controller/Admin.php
@@ -3,4 +3,21 @@ namespace Box\Mod\Pterodactyl\Controller;
 
 class Admin implements \Box\InjectionAwareInterface
 {
+  	protected $di;
+
+    /**
+     * @param mixed $di
+     */
+    public function setDi($di)
+    {
+        $this->di = $di;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDi()
+    {
+        return $this->di;
+    }
 }
