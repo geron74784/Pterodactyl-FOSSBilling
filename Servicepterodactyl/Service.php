3fb7f4bf2ca8cd1c92d9510e29b6c0505794d523
fabpot-del <93833001+fabpot-del@users.noreply.github.com>
2023-01-08 21:07:16 +0100

    Update Service.php

--- Servicepterodactyl/Service.php
+++ Servicepterodactyl/Service.php
@@ -22,6 +22,34 @@ class Service implements \Box\InjectionAwareInterface
     }
   
     /**
+     * Method to install module. In most cases you will provide your own
+     * database table or tables to store extension related data.
+     *
+     * If your extension is not very complicated then extension_meta
+     * database table might be enough.
+     *
+     * @return bool
+     * @throws \Box_Exception
+     */
+    public function install()
+    {
+	$sql = "
+        CREATE TABLE IF NOT EXISTS `service_pterodactyl` (
+            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+	    `client_id` bigint(20) DEFAULT NULL,
+	    `order_id` bigint(20) DEFAULT NULL,
+            `created_at` varchar(35) DEFAULT NULL,
+            `updated_at` varchar(35) DEFAULT NULL,
+            PRIMARY KEY (`id`),
+	    KEY `client_id_idx` (`client_id`)
+            ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
+        $this->di['db']->exec($sql);
+
+        //throw new \Box_Exception("Throw exception to terminate module installation process with a message", array(), 123);
+        return true;
+    }
+	
+    /**
      * Method to uninstall module.
      *
      * @return bool
@@ -34,4 +62,26 @@ class Service implements \Box\InjectionAwareInterface
         //throw new \Box_Exception("Throw exception to terminate module uninstallation process with a message", array(), 124);
         return true;
     }
+
+    /**
+     * @param \Model_ClientOrder $order
+     * @return void
+     */
+    public function create($order)
+    {
+	$config = json_decode($order->config, 1);
+
+		
+        $product = $this->di['db']->getExistingModelById('Product', $order->product_id, 'Product not found');
+
+        $model                	= $this->di['db']->dispense('service_pterodactyl');
+        $model->client_id     	= $order->client_id;
+	$model->order_id     	= $order->id;
+        $model->created_at    	= date('Y-m-d H:i:s');
+        $model->updated_at    	= date('Y-m-d H:i:s');
+        $this->di['db']->store($model);
+
+        return $model;
+    }
+	
 }
